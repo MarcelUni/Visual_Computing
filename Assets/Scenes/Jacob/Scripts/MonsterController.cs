722852a7cfd66f0e02abbb248f391e01e9c32e77
JacobVigsoe <eh66xs@student.aau.dk>
2024-10-02 14:27:36 +0200

    enemy detection

---
+++ Assets/Scenes/Jacob/Scripts/MonsterController.cs
@@ -0,0 +1,155 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MonsterController : MonoBehaviour
+{
+    [SerializeField] private float speed = 5f;
+    [SerializeField] private float detectionDistance = 20f;
+    [SerializeField] private GameObject killBox;
+    public float waitToStartRoaming = 5f;
+    public float killRadius = 5f;
+    private Animator animator;
+    private TestSneak testSneak;
+    public MonsterState currentState;
+
+    public Transform[] patrolPoints;
+
+    public bool detectedLumi = false;
+    public bool gotLatestPosition = false;
+    public enum MonsterState
+    {
+        Idle,
+        Roaming,
+        Investigate
+    }
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        testSneak = GetComponent<TestSneak>();
+        float sphereRadius = GetComponent<SphereCollider>().radius;   
+        currentState = MonsterState.Roaming;
+        sphereRadius = detectionDistance;
+    }
+
+    private void Update()
+    {
+        
+        switch(currentState)
+        {
+            case MonsterState.Investigate:
+                //spil noget animation mens den går til destination fx. animator.Play("Walk");
+                break;
+            case MonsterState.Idle:
+                //spil noget animation fx. animator.Play("Idle") eller en kigge animation;
+                break;
+            case MonsterState.Roaming:
+                Roam();
+                //spil noget animation fx. animator.Play("walk");
+                break;
+
+        }
+       
+            
+
+
+    }
+
+    private int currentPatrolPointIndex = 0;
+
+    private void Roam()
+    {
+        if (patrolPoints.Length == 0)
+        {
+            Debug.LogError("No patrol points found");
+            return;
+        }
+
+        Transform currentPatrolPoint = patrolPoints[currentPatrolPointIndex];
+        GetComponent<NavMeshAgent>().SetDestination(currentPatrolPoint.position);
+
+        if (Vector3.Distance(currentPatrolPoint.position, transform.position) <= 1)
+        {
+            currentPatrolPointIndex++;
+            if (currentPatrolPointIndex >= patrolPoints.Length)
+            {
+                currentPatrolPointIndex = 0;
+            }
+        }
+
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") )
+        {
+            GetComponent<NavMeshAgent>().stoppingDistance = 5;
+            Investigate(other.transform, false);
+            StartCoroutine(PatrolWait());
+
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player") && other.GetComponent<TestSneak>().isSneaking == false)
+        {
+            GetComponent<NavMeshAgent>().stoppingDistance = 1;
+            Investigate(other.transform, true);
+            detectedLumi = true;
+
+        }
+        if (other.CompareTag("Player") && other.GetComponent<TestSneak>().isWalking == true)
+        {
+            GetComponent<NavMeshAgent>().stoppingDistance = 1;
+            Investigate(other.transform, true);
+        }
+        else if (other.CompareTag("Player") && other.GetComponent<TestSneak>().isSneaking == true)
+        {
+            detectedLumi = false;
+        }
+
+    }
+
+    private void Investigate(Transform target, bool lumiDetected)
+    {
+        if (lumiDetected == true)
+        {
+            detectedLumi = true;
+            // play sound for suspence
+        }
+        else if (lumiDetected == false)
+        {
+            detectedLumi = false;
+        }
+        GetComponent<NavMeshAgent>().SetDestination(target.position);
+        currentState = MonsterState.Investigate;
+        if (target.position == transform.position) // har arrrived at destination
+        {
+            // måske spille en look around animation
+            currentState = MonsterState.Idle;
+
+        }
+        if (Vector3.Distance(target.position, killBox.transform.position) <= killRadius && detectedLumi == true)
+        {
+            Kill();
+        }
+    }
+
+    private IEnumerator PatrolWait()
+    {
+        yield return new WaitForSeconds(waitToStartRoaming);
+        GetComponent<NavMeshAgent>().stoppingDistance = 0;
+        currentState = MonsterState.Roaming;
+    }
+
+    private void Kill()
+    {
+        Debug.Log("Lumi is dead");
+        // call method from charactercontroller to kill lumi and maybe call game manager to restart level
+    }
+}
+
